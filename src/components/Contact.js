32b86a181323e10cb23e5ce2674db4a722ac7782
Dhawal M <dhawalmehta91@gmail.com>
2018-07-12 00:11:08 +0530

    php mailer added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,50 @@
 import React from 'react'
+import $ from 'jquery';
 
 class Contact extends React.Component {
   constructor(){
     super();
 
     this.state = {
-      name: "name",
-      email: "email",
-      message: "message",
+      name: "",
+      email: "",
+      message: "",
+      sent:false,
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   handleInputChange(e){
-    const target = e.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const name = this.state.name;
+    const email = this.state.email;
+    const message = this.state.message;
+
+    let _this = this;
+
+    $.ajax({
+      type: 'POST',
+      url: '../mail/contact_me.php',
+      data: {name, email, message},
+      success: function (response) {
+        console.dir(response);
+        _this.setState({
+          name: "",
+          email: "",
+          message: "",
+          sent: true,
+        });
+      },
+      fail: function (err) {
+        console.dir(err);
+      }
     });
   }
 
@@ -26,28 +52,36 @@ class Contact extends React.Component {
     return <div className="container">
       <div className="row justify-content-center">
         <div className="col-sm-8">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <input type="hidden" value="prayer" />
             <fieldset>
               <legend>Contact me.</legend>
 
               <div className="control">
-                <input type="text" name={this.state.name} id="name" placeholder="What should I call you?" onChange={this.handleInputChange} autoFocus required autoComplete="name"/>
+                <input type="text" name="name" id="name" value={this.state.name} placeholder="What should I call you?" onChange={this.handleInputChange} autoFocus required autoComplete="name"/>
                 <label htmlFor="name">Name</label>
               </div>
 
               <div className="control">
-                <input type="email" id="email" name={this.state.email} placeholder="Where can I reach you?" onChange={this.handleInputChange} required  autoComplete="email"/>
+                <input type="email" id="email" name="email" value={this.state.email} placeholder="Where can I reach you?" onChange={this.handleInputChange} required autoComplete="email"/>
                 <label htmlFor="email">e-mail</label>
               </div>
 
               <div className="control">
-                <textarea id="message" name={this.state.message} placeholder="What's on your mind?" onChange={this.handleInputChange} required>
+                <textarea id="message" name="message" value={this.state.message} placeholder="What's on your mind?" onChange={this.handleInputChange} required>
                 </textarea>
                 <label htmlFor="message">Message</label>
               </div>
 
               <input type="submit" value="send" />
+
+              {this.state.sent?(
+                <span className="mx-3 text-success">
+                  Your message has been sent successfully!
+                </span>
+              ):(
+                ""
+              )}
 
             </fieldset>
 
