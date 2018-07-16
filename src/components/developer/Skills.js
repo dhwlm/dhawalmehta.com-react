f04692d7dd188ce650a233317d14315da7fb8ee3
Dhawal M <dhawalmehta91@gmail.com>
2018-07-16 15:38:25 +0530

    project title fix and skill word wrap fix

--- src/components/developer/Skills.js
+++ src/components/developer/Skills.js
@@ -20,49 +20,49 @@ class Skills extends React.Component {
 
             <div className="row mt-5 scroll-reveal__sequence-container">
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>HTML/CSS &mdash; <span className="text-secondary">9/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>JavaScript/JQuery &mdash; <span className="text-secondary">8/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>React &mdash; <span className="text-secondary">7/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>PHP/MySQL &mdash; <span className="text-secondary">7/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>Laravel/OpenCart/Wordpress/CodeIgniter &mdash; <span className="text-secondary">8/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>Bootstrap 3/4 &mdash; <span className="text-secondary">9/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>GIT &mdash; <span className="text-secondary">9/10</span></p>
                 </div>
               </div>
 
               <div className="col-12 mb-4 scroll-reveal--sequenced">
-                <div className="border border-secondary p-2">
+                <div className="border border-secondary p-2 skill-block">
                   <p>PHOTOSHOP &mdash; <span className="text-secondary">6/10</span></p>
                 </div>
               </div>
