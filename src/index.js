1ef61f01e5033c0af16372458e2bb0bf6651c685
Dhawal M <dhawalmehta91@gmail.com>
2019-10-02 12:23:15 +0530

    serviceworker added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.min.js';
 import 'imagesloaded';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
@@ -15,4 +15,4 @@ ReactDOM.render(
     </ScrollToTop>
   </BrowserRouter>
   , document.getElementById('root'));
-  // registerServiceWorker();
+  registerServiceWorker();
